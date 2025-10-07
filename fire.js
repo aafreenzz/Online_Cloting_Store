ef6f0d35cddaa9f3e16ed6abb253348d54934461
Aafreen <poudelaafreen@gmail.com>
2025-10-07 17:54:55 +0545

    Add files via upload

---
+++ fire.js
@@ -0,0 +1,10 @@
+function toggleSidebar() {
+  let sidebar = document.getElementById("sidebar");
+  if (sidebar.style.left === "0px") {
+    sidebar.style.left = "-200px";
+  } else {
+    sidebar.style.left = "0px";
+  }
+}
+// Select all images inside #products container
+
